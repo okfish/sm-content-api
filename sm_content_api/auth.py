3502d9c29f2fb729b68f1863fdeca04a9e315d9f
okfish <okfish@yandex.ru>
2024-05-04 04:13:17 +0300

    Sometimes multiple coroutines doing request has catch a 401 error and need to refresh token together. It leads to multiple requests for a token. So, this is a try to avoid that cases, waiting a random time between retries

--- sm_content_api/auth.py
+++ sm_content_api/auth.py
@@ -1,4 +1,5 @@
 import asyncio
+from random import random
 import functools
 from aiohttp_retry.retry_options import ExponentialRetry
 
@@ -7,7 +8,7 @@ from .api import AuthenticationApi
 
 
 # logger = logging.getLogger('sm_content_api')
-retry_opts = ExponentialRetry(statuses={401, }, start_timeout=1)
+retry_opts = ExponentialRetry(statuses={401, 403}, start_timeout=1)
 
 
 def refresh_expired_token(retry_options: ExponentialRetry = retry_opts):
@@ -28,13 +29,15 @@ def refresh_expired_token(retry_options: ExponentialRetry = retry_opts):
             current_attempt = 0
 
             while True:
-                logger.info(f"Attempt {current_attempt + 1} out of {max_retries}")
+                logger.info(f"Attempt {current_attempt + 1} out of {max_retries}. Host: {config.host}")
 
                 current_attempt += 1
                 try:
                     res = await func(self, *args, **kwargs)
+                    logger.debug(f"Response {res}")
 
-                    if hasattr(res, 'data'):
+                    # some responses does not contain  data property and may be empty (None)
+                    if hasattr(res, 'data') or res is None:
                         return res
 
                     if current_attempt == max_retries:
@@ -48,14 +51,24 @@ def refresh_expired_token(retry_options: ExponentialRetry = retry_opts):
                         raise e
 
                     if e.status in retry_options.statuses:
-                        logger.info(f"Refreshing expired token.  "
-                                    f"Old was {old_token[:8] if old_token and len(old_token) > 8 else 'None'}")
-                        auth_api = AuthenticationApi(self.api_client)
-                        resp = await auth_api.get_token(config.client_id,
-                                                        config.client_secret,
-                                                        config.grant_type)
-                        if resp.token_type == 'Bearer':
-                            self.api_client.configuration.access_token = resp.access_token
+
+                        if old_token == config.access_token:
+                            # wait random time and check if other coroutines already refreshed token
+                            _wait = random() * 2
+                            logger.debug(f'Waiting random time before refreshing token (t={_wait})')
+                            await asyncio.sleep(_wait)
+
+                        if old_token == config.access_token:
+                            logger.info(f"Refreshing expired token.  "
+                                        f"Old was {old_token[:8] if old_token and len(old_token) > 8 else 'None'}")
+                            auth_api = AuthenticationApi(self.api_client)
+                            resp = await auth_api.get_token(config.client_id,
+                                                            config.client_secret,
+                                                            config.grant_type)
+                            if resp.token_type == 'Bearer':
+                                config.access_token = resp.access_token
+                        else:
+                            logger.info(f"Token seems to be already refreshed. Skipping refresh.")
 
                     elif e.status >= 500:
                         raise e
